97350a7911e37cddef31a5e1a1ee4ba480831f1a
NRotondaro <niksonrotondaro1@gmail.com>
2022-05-29 19:52:26 -0300

    feat(header): installed and implemented next-auth with reddit provider

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,8 +14,11 @@ import {
   MenuIcon,
   SearchIcon,
 } from '@heroicons/react/solid'
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 export const Header = () => {
+  const { data: session } = useSession()
+
   return (
     <div className="sticky top-0 z-50 flex bg-white px-4 py-2 shadow-sm">
       <div className="relative h-10 w-20 flex-shrink-0 cursor-pointer">
@@ -56,17 +59,44 @@ export const Header = () => {
         <MenuIcon className="icon" />
       </div>
 
-      <div className="hidden lg:flex items-center space-x-2 border border-gray-100 p-2 cursor-pointer">
-        <div className="relative h-5 w-5 flex-shrink-0">
-          <Image
-            src="https://logoeps.com/wp-content/uploads/2014/09/52053-reddit-logo-icon-vector-icon-vector-eps.png"
-            alt="outline-logo"
-            layout="fill"
-            objectFit="contain"
-          />
+      {session ? (
+        <div
+          onClick={() => signOut()}
+          className="hidden lg:flex items-center space-x-2 border border-gray-100 p-2 cursor-pointer"
+        >
+          <div className="relative h-5 w-5 flex-shrink-0">
+            <Image
+              src="https://logoeps.com/wp-content/uploads/2014/09/52053-reddit-logo-icon-vector-icon-vector-eps.png"
+              alt="outline-logo"
+              layout="fill"
+              objectFit="contain"
+            />
+          </div>
+
+          <div className="flex-1 text-xs">
+            <p className="truncate">{session?.user?.name}</p>
+            <p className="text-gray-400">1 Karma</p>
+          </div>
+
+          <ChevronDownIcon className="h-5 flex-shrink-0 text-gray-400" />
         </div>
-        <p className="text-gray-400">Sign In</p>
-      </div>
+      ) : (
+        <div
+          onClick={() => signIn()}
+          className="hidden lg:flex items-center space-x-2 border border-gray-100 p-2 cursor-pointer"
+        >
+          <div className="relative h-5 w-5 flex-shrink-0">
+            <Image
+              src="https://logoeps.com/wp-content/uploads/2014/09/52053-reddit-logo-icon-vector-icon-vector-eps.png"
+              alt="outline-logo"
+              layout="fill"
+              objectFit="contain"
+            />
+          </div>
+
+          <p className="text-gray-400">Sign In</p>
+        </div>
+      )}
     </div>
   )
 }
---
+++ types/environment.d.ts
@@ -0,0 +1,6 @@
+namespace NodeJS {
+  interface ProcessEnv extends NodeJS.ProcessEnv {
+    REDDIT_CLIENT_ID: string
+    REDDIT_CLIENT_SECRET: string
+  }
+}
