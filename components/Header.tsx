6d44d2bcc1fb1ed46aeac796a3eec030131e8581
NRotondaro <niksonrotondaro1@gmail.com>
2022-05-30 00:14:11 -0300

    feat(stepzen): implemented stepzen with graphql and some feat on the header

--- components/Header.tsx
+++ components/Header.tsx
@@ -20,7 +20,7 @@ export const Header = () => {
   const { data: session } = useSession()
 
   return (
-    <div className="sticky top-0 z-50 flex bg-white px-4 py-2 shadow-sm">
+    <div className="sticky top-0 z-50 items-center flex bg-white px-4 py-2 shadow-sm">
       <div className="relative h-10 w-20 flex-shrink-0 cursor-pointer">
         <Image
           layout="fill"
