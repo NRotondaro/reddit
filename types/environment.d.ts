6e7e2840da0c7c2ed82158e15ff25eb342f45818
NRotondaro <niksonrotondaro1@gmail.com>
2022-05-30 00:37:22 -0300

    feat(apollo-client): integrated the db using apollo client

--- types/environment.d.ts
+++ types/environment.d.ts
@@ -2,5 +2,8 @@ namespace NodeJS {
   interface ProcessEnv extends NodeJS.ProcessEnv {
     REDDIT_CLIENT_ID: string
     REDDIT_CLIENT_SECRET: string
+    NEXT_PUBLIC_STEPZEN_KEY: string
+    NEXTAUTH_SECRET: string
+    NEXTAUTH_URL: string
   }
 }
